4069c260dee8b2805c53da441b387324fdb6a35d
YasserHage <yhage@betait.com.br>
2021-07-02 01:20:30 -0300

    Inicializando Spring pelo Spring Initializr

---
+++ src/main/java/com/crudfy/CrudfyApplication.java
@@ -0,0 +1,13 @@
+package com.crudfy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudfyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudfyApplication.class, args);
+	}
+
+}
