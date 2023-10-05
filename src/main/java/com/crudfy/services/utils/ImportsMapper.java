94267adcdbf8d7e843e2b49ecb71729c0ce769aa
YasserHage <yasser.hage1@gmail.com>
2023-10-05 20:17:07 -0300

    [#15] New import logic to support sub entities with complex types

--- src/main/java/com/crudfy/services/utils/ImportsMapper.java
+++ src/main/java/com/crudfy/services/utils/ImportsMapper.java
@@ -1,5 +1,6 @@
 package com.crudfy.services.utils;
 
+import org.codehaus.plexus.util.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ResourceLoader;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -8,10 +9,7 @@ import org.springframework.stereotype.Component;
 import javax.annotation.PostConstruct;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @Component
 public class ImportsMapper {
@@ -19,7 +17,7 @@ public class ImportsMapper {
     @Autowired
     private ResourceLoader loader;
 
-    private Map<String, List<String>> imports = new HashMap<>();
+    private Map<String, String> imports = new HashMap<>();
 
     @PostConstruct
     private void populate() throws IOException {
@@ -28,7 +26,26 @@ public class ImportsMapper {
         imports.putAll(mapper.readValue(importResource, Map.class));
     }
 
-    public List<String> getImports(String type) {
-        return imports.getOrDefault(type, null);
+    public List<String> getImports(String fullType) {
+        List<String> types = new ArrayList<>();
+        if (fullType.contains("<")) {
+            String mainType = fullType.substring(0, fullType.indexOf('<'));
+            String[] subTypes = fullType.substring(fullType.indexOf('<') + 1, fullType.indexOf('>')).split(",");
+
+            addImport(mainType, types);
+            for (String type : subTypes) {
+                addImport(fullType, types);
+            }
+        } else {
+            addImport(fullType, types);
+        }
+        return types;
+    }
+
+    private void addImport(String type, List<String> list) {
+        String typeImport = imports.get(type);
+        if (StringUtils.isNotBlank(typeImport)) {
+            list.add(typeImport);
+        }
     }
 }
